c6a72e2deb7909d9a9a0ad08e3e80f7fd527ba72
Claire Muller <clairermuller@gmail.com>
2018-12-18 03:38:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
-/*
-function shout(string) {
-  return string.toUpperCase();
-}
-*/
-
 const shout = (string) => {
   return string.toUpperCase();
 }
-
-/*
-function whisper(string) {
-  return string.toLowerCase();
-}
-*/
 
 const whisper = (string) => {
   return string.toLowerCase();
 }
 
-
 const logShout = (string) => {
-  
   console.log(string.toUpperCase());
 }
 
-/*
-describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logShout('hello')
-
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
-  })
-})
-*/
