5fdf8226be1dfb158933e10798628a2b01197350
Claire Muller <clairermuller@gmail.com>
2018-12-18 04:20:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const logWhisper = (string) => {
 const sayHiToGrandma = (string) => {
   if (string === string.toLowerCase()) {
     return "I can't hear you!";
-  } else if (string === .toUpperCase()) {
+  } else if (string === string.toUpperCase()) {
     return "YES INDEED!";
   } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
