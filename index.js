07e497cd3739e8a7a384d55a92b5dd07383ad2fa
Claire Muller <clairermuller@gmail.com>
2018-12-18 03:20:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+/*
 function shout(string) {
   return string.toUpperCase();
 }
+*/
 
-const shout = function(string)
+const shout = function(string) {
+  return string.toUpperCase();
+}
 
 function whisper(string) {
   return string.toLowerCase();
