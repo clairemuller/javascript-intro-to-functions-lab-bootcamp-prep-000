f4cb4ca74b5b795d800f40d0d5ae142d6f83af96
Claire Muller <clairermuller@gmail.com>
2018-12-18 03:29:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,4 +19,20 @@ const whisper = (string) => {
 }
 
 
+const logShout = (string) => {
+  
+}
+
+
+describe('logShout(string)', function() {
+  it('calls console.log() its one argument in all caps', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
+})
 
